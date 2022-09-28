5a0038054c9c2b77397f741f0936fce0b2b273d9
Sam1981 <114189636+Sam1981-arch@users.noreply.github.com>
2022-09-27 21:27:52 -0500

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
@@ -7,11 +8,22 @@ Route::group(['prefix' => 'Users', 'controller' => UserController::class], funct
 
 	Route::get('/GetAllUsers','getAllUsers');
 	Route::get('/GetUserById/{user}','getUserById');
-	Route::get('/SaveUser','saveUser');
+	Route::get('/SaveUser','saveUser')->middleware((['role:admin|reginal.ta.manager|ta.manager|ta.lead|ta.officer|ta.backoffice|ta.assistant']));
 	Route::get('/UpdateUser/{user}','updateUser');
 	Route::get('/DeleteUser/{user}','deleteUser');
 
 
+	
+});
+
+Route::get('/test', function (){
+
+	$user = User::find(1);
+	$user->assignRole('admin');
+	$user = User::find(2);
+	$user->assingnRole('user');
+  //$role = Role::create(['name' => 'user']);
+	//return $role;
 });
 
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
@@ -13,4 +15,23 @@ Route::get('/', function () {
     return view('index');
 });
 
+
+Route::get('/test', function (){
+
+	$user = User::find(1);
+	$user->assignRole('admin');
+	$user = User::find(2);
+	$user->assingnRole('user');
+  //$role = Role::create(['name' => 'user']);
+	//return $role;
+});
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/garments', [App\Http\Controllers\GarmentsController::class, 'garments'])->name('garments');
+
+Route::get('/category', [App\Http\Controllers\CategotyController::class, 'category'])->name('category');
+
+Route::get('/brand', [App\Http\Controllers\BrandController::class, 'brand'])->name('brand');
+
+
+
